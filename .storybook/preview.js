b78880754c18658705a8d9c129129d09751613d2
Ariga914 <pmh69ess@s.okayama-u.ac.jp>
2023-03-25 01:11:42 +0900

    feat; constructured initial environment

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../src/config/theme';
+import { addDecorator } from '@storybook/react';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+addDecorator((story) => (
+  <ThemeProvider theme={theme}>
+    {story()}
+  </ThemeProvider>
+))
