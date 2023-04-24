782d0bb74714b7ab0b508e08585a45943317c73b
Ariga914 <pmh69ess@s.okayama-u.ac.jp>
2023-04-25 02:59:15 +0900

    feat: created new consts for typography

---
+++ src/types/style.type.ts
@@ -0,0 +1,34 @@
+export type FontFamily =
+  | 'Sawarabi Gothic'
+  | 'Kaisei Opti'
+  | 'sans-serif'
+  | 'serif';
+
+export type FontWeight = 400 | 500 | 700;
+
+export type FontWeightKey = 'regular' | 'medium' | 'bold';
+
+export type LineHeight = '110%' | '140%';
+
+export type LineHeightKey = 'heading' | 'body';
+
+export type LetterSpacing = '-5%' | '0' | '120%';
+
+export type LetterSpacingKey = 'default' | 'decreased' | 'increased';
+
+export type FontSize = 16 | 48.329 | 39.063 | 31.25 | 25 | 20 | 13.6 | 10.4;
+
+export type FontSizeKey =
+  | 'body'
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'sm'
+  | 'xs';
+
+export type TextTransform = 'uppercase' | 'none';
+
+export type TextDecoration = 'underline' | 'none';
