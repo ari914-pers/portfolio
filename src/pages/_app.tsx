6ae66b8d7130c4751e0cec5ce4a2d7e365a11f88
Ariga914 <pmh69ess@s.okayama-u.ac.jp>
2023-06-28 14:55:27 +0900

    fix: 新しいheadタグを反映させるためにNext appをconfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import CustomThemeProvider from '@/config/styles/CustomThemeProvider';
 import GlobalStyleProvider from '@/config/styles/GlobalStyleProvider';
 import { appWithTranslation } from 'next-i18next';
 import LayoutRegular from '@/components/layout/LayoutRegular';
+import NextHead from '@/config/app/NextHead';
+import Head from 'next/head';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -21,6 +23,9 @@ function MyApp(props: MyAppProps) {
     <CacheProvider value={emotionCache}>
       <CustomThemeProvider>
         {GlobalStyleProvider}
+        <Head>
+          <NextHead />
+        </Head>
         <LayoutRegular>
           <Component {...pageProps} />
         </LayoutRegular>
