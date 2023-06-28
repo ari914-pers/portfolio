2227d3951d04db46bb82f8ce932dbaf8ffab9c25
Ariga914 <pmh69ess@s.okayama-u.ac.jp>
2023-06-28 18:05:57 +0900

    feat: FAQページ作成

---
+++ src/features/faq/FaqListView.tsx
@@ -0,0 +1,21 @@
+import BaseCard from '@/components/molecules/surfaces/BaseCard';
+import { ComponentProps, FC } from 'react';
+import FaqView from '../home/faq/FaqView';
+import { useTranslation } from 'react-i18next';
+
+type FaqListViewProps = { faqs: ComponentProps<typeof FaqView>['faqs'] | null };
+
+const FaqListView: FC<FaqListViewProps> = ({ faqs }) => {
+  const { t } = useTranslation('home');
+
+  return faqs ? (
+    <BaseCard
+      title={t('home.faq.title')}
+      description={t('home.faq.description')}
+    >
+      <FaqView faqs={faqs} isUsedOnHome={false} />
+    </BaseCard>
+  ) : null;
+};
+
+export default FaqListView;
