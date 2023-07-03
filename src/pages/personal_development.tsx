44ed3cb1d58521cb837bb562440497880a24ffff
Ariga914 <pmh69ess@s.okayama-u.ac.jp>
2023-07-03 14:16:28 +0900

    fix: page COMPのcms clientの呼び出しを修正　バグ改修

--- src/pages/personal_development.tsx
+++ src/pages/personal_development.tsx
@@ -5,6 +5,8 @@ import { getEntries } from '@/utils/cmsUtils';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { isNull } from 'lodash';
 import PersonalDevelopmentListView from '@/features/personal_development/PersonalDevelopmentListView';
+import { DEPTH_REF_BY_PERSONAL_DEVELOPMENT } from '@/consts/cms';
+import { ContentGetQueryParam } from '@/types/cms.type';
 
 type PersonalDevelopmentPageP = {
   personalDevelopments:
@@ -25,9 +27,10 @@ export default PersonalDevelopmentPage;
 export const getStaticProps: GetStaticProps = async (
   context
 ): Promise<GetStaticPropsResult<PersonalDevelopmentPageP>> => {
-  const personalDevelopments = await getEntries<IPersonalDevelopmentFields>(
-    'personal_development'
-  );
+  const personalDevelopments = await getEntries<
+    IPersonalDevelopmentFields,
+    ContentGetQueryParam
+  >('personal_development', { include: DEPTH_REF_BY_PERSONAL_DEVELOPMENT });
 
   return {
     props: {
